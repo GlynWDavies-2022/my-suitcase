86cea45c3a5c8ac5dbdc1643beca4318efacba0d
Glyn Davies <glynwdavies@gmail.com>
2026-08-21 13:45:06 +0100

    Added form control.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,37 @@
+import { useState } from 'react';
+
 function Form() {
+    const [description, setDescription] = useState('');
+    const [quantity, setQuantity] = useState(0);
+
     function handleSubmit(e) {
         e.preventDefault();
+        if (!description) return;
+        const newId = Math.floor(Math.random() * 10000);
+        console.log(newId);
+        const newItem = { id: newId, description, quantity, packed: false };
+        console.log(newItem);
+        setDescription('');
+        setQuantity(0);
     }
 
     return (
         <form className='form' onSubmit={handleSubmit}>
             <label>What do you need for your trip?&nbsp;</label>
-            <select className='quantity'>
+            <select className='quantity' value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
                 {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                     <option value={num} key={num}>
                         {num}
                     </option>
                 ))}
             </select>
-            <input className='item' type='text' placeholder='Item...' />
+            <input
+                className='item'
+                type='text'
+                placeholder='Item...'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
             <button className='add'>Add</button>
         </form>
     );
